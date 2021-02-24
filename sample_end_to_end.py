0c316ffb125670721ee6f2768be860215a6b7dcb
Luis Fernando <27741646+lmachadopolettivalle@users.noreply.github.com>
2021-02-23 22:17:51 -0500

    Improve sample end to end plot

--- sample_end_to_end.py
+++ sample_end_to_end.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 from filter_data_helpers import get_halos_based_on_filters, get_profiles
 
+HUBBLE = 0.6711
+
 # Select halos based on mass cut and simulation ID
 halo_ids_result = get_halos_based_on_filters(
-    list_of_inequality_filters=[("M_Crit200", 100, 1e3)],
+    list_of_inequality_filters=[("M_Crit200", 100/HUBBLE, 1e3/HUBBLE)],
     list_of_equality_filters=[("simulation_id", ["sample simulation ID"])]
 )
 print(halo_ids_result)
@@ -24,10 +26,26 @@ profiles_result = get_profiles(
 
 print(profiles_result)
 
+# Rescale data to common units
+profiles_result["radius"] *= 1 / (HUBBLE * 1e3) # Convert to Mpc
+profiles_result["property_value"] *= 1e10 * HUBBLE * HUBBLE # Convert to Msun * kpc^-3
+
 # Plot profiles for different halos
 profiles_result = profiles_result.pivot(index="radius", columns="halo_unique_id", values="property_value")
 print(profiles_result)
 
-profiles_result.plot(logx=True)
+# Filter radial values
+profiles_result = profiles_result[(profiles_result.index > 0.01) & (profiles_result.index < 3)]
+
+
+# Compute percentiles of profile data
+profiles_16 = profiles_result.quantile(q=0.16, axis="columns")
+profiles_84 = profiles_result.quantile(q=0.84, axis="columns")
+profiles_median = profiles_result.quantile(q=0.5, axis="columns")
+
+
+profiles_16.plot(logx=True)
+profiles_84.plot(logx=True)
+profiles_median.plot(logx=True)
 
 plt.show()
