7bf202d1d1d7a6e56993c2ddc1f807dc35feafea
Luis Fernando <luisfernando.machadopoletti@gmail.com>
2021-06-28 23:51:27 +0200

    Include helper queries for subhalos and mergertree tables

--- sample_end_to_end.py
+++ sample_end_to_end.py
@@ -22,6 +22,7 @@ print(simulation_IDs)
 profiles_result = get_profiles(
     list_of_halo_ids=halo_IDs,
     list_of_simulation_ids=simulation_IDs,
+    list_of_snapshots=[33],
     list_of_properties=["gas_density"] # See options in illstack_helpers.py
 )
 
