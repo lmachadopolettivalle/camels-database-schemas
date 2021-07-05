f90a88ca62f06049a8f59045a5d0a5a07a868a9b
Luis Fernando <luisfernando.machadopoletti@gmail.com>
2021-07-05 23:33:52 +0200

    Make filter functions more user-friendly, improve sample script

--- filter_data_helpers.py
+++ filter_data_helpers.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+from numpy import where
 from database_helpers import set_database_filename, execute_query
 
 import pandas as pd
@@ -5,7 +8,7 @@ import pandas as pd
 DATABASE_FILENAME = "sample.db"
 set_database_filename(DATABASE_FILENAME)
 
-def get_all_simulation_details():
+def get_all_simulation_details() -> pd.DataFrame:
     query = f"""
     SELECT 
     *
@@ -16,7 +19,52 @@ def get_all_simulation_details():
     return pd.DataFrame(data)
 
 
-def get_halos_based_on_filters(list_of_inequality_filters, list_of_equality_filters):
+INEQUALITY_FILTER = tuple[str, float, float] # e.g. ("M_Crit200", 30, 200)
+EQUALITY_FILTER = tuple[str, list] # e.g. ("simulation_unique_id", ["IllustrisTNG_1P_22", "SIMBA_LH_1"])
+def _build_inequality_clause(
+        list_of_inequality_filters: list[INEQUALITY_FILTER]
+    ) -> list[str]:
+
+    where_clauses_list = []
+
+    for (column_name, lower_limit, upper_limit) in list_of_inequality_filters:
+        if lower_limit is not None:
+            where_clauses_list.append(f"{column_name} >= {lower_limit}")
+        if upper_limit is not None:
+            where_clauses_list.append(f"{column_name} <= {upper_limit}")
+
+    return where_clauses_list
+
+def _build_equality_clause(
+        list_of_equality_filters: list[EQUALITY_FILTER]
+    ) -> list[str]:
+
+    where_clauses_list = []
+
+    for (column_name, values) in list_of_equality_filters:
+        if isinstance(values[0], str):
+            values = [f"'{v}'" for v in values]
+        else:
+            values = [str(v) for v in values]
+        
+        where_clauses_list.append(f"{column_name} IN ({','.join(values)})")
+
+    return where_clauses_list
+
+def get_halos_based_on_filters(
+        list_of_inequality_filters: list[INEQUALITY_FILTER],
+        list_of_equality_filters: list[EQUALITY_FILTER]
+    ) -> pd.DataFrame:
+    """Given lists of equalities and inequalities,
+    return the corresponding FoF halo entries.
+    
+    Keyword Arguments:
+    list_of_inequality_filters: list of filters. Each filter contains a property name, a minimum and a maximum value.
+    - e.g. for one filter: ("M_Crit200", 30, 200)
+    list_of_equality_filters: list of filters. Each filter contains a property name and a list of accepted values.
+    - e.g. for one filter: ("simulation_unique_id", ["IllustrisTNG_1P_22"])
+    """
+
     query = """
     SELECT
     *
@@ -24,18 +72,8 @@ def get_halos_based_on_filters(list_of_inequality_filters, list_of_equality_filt
     """
 
     query_where_clauses = []
-
-    for (column_name, lower_limit, upper_limit) in list_of_inequality_filters:
-        if lower_limit is not None:
-            query_where_clauses.append(f"{column_name} >= {lower_limit}")
-        if upper_limit is not None:
-            query_where_clauses.append(f"{column_name} <= {upper_limit}")
-
-    for (column_name, values) in list_of_equality_filters:
-        if isinstance(values[0], str):
-            values = [f"'{v}'" for v in values]
-        
-        query_where_clauses.append(f"{column_name} IN ({','.join(values)})")
+    query_where_clauses.extend(_build_equality_clause(list_of_equality_filters))
+    query_where_clauses.extend(_build_inequality_clause(list_of_inequality_filters))
 
     if len(query_where_clauses) != 0:
         query += " WHERE " + " AND ".join(query_where_clauses)
@@ -45,76 +83,135 @@ def get_halos_based_on_filters(list_of_inequality_filters, list_of_equality_filt
 
 
 
-def get_profiles(list_of_halo_ids, list_of_simulation_ids, list_of_snapshots, list_of_properties):
-    query = """
-    SELECT
-    *
-    FROM profiles
+def get_profiles_for_halos(
+        list_of_halos: Union[pd.DataFrame, list[dict]],
+        list_of_properties: list[str]
+    ) -> pd.DataFrame:
+    """Given a list of halos and a list of desired properties,
+    return the corresponding radial profiles.
+    
+    Keyword Arguments:
+    list_of_halos -- A dict, or pandas.DataFrame, containing the following keys:
+    - ID: halo IDs, e.g. 0
+    - simulation_unique_id: e.g. IllustrisTNG_1P_22
+    - snapshot: e.g. 33
+    list_of_properties -- list of strings, each of which match a property stored in the 'profiles' table.
+    - e.g. ["gas_density", "metallicity"]
     """
 
-    query_where_clauses = []
+    # If given a list, convert to pandas.DataFrame, for ease of iteration
+    if isinstance(list_of_halos, list):
+        list_of_halos = pd.DataFrame(list_of_halos)
 
-    if len(list_of_halo_ids) != 0:
-        halo_in_clause = "ID IN (" + ",".join([f"'{h}'" for h in list_of_halo_ids]) + ")"
-        query_where_clauses.append(halo_in_clause)
-    if len(list_of_simulation_ids) != 0:
-        simulation_in_clause = "simulation_unique_id IN (" + ",".join([f"'{s}'" for s in list_of_simulation_ids]) + ")"
-        query_where_clauses.append(simulation_in_clause)
-    if len(list_of_snapshots) != 0:
-        snapshot_in_clause = "snapshot IN (" + ",".join([f"'{s}'" for s in list_of_snapshots]) + ")"
-        query_where_clauses.append(snapshot_in_clause)
-    if len(list_of_properties) != 0:
-        properties_in_clause = "property_key IN (" + ",".join([f"'{p}'" for p in list_of_properties]) + ")"
-        query_where_clauses.append(properties_in_clause)
+    # SQL cannot handle a direct query with several (>1000) conditions.
+    # As a result, we perform several queries,
+    # one for each (simulation_unique_id, snapshot) combination,
+    # and combine the results in the end before returning.
+    simulation_snapshot_combinations = list_of_halos[["simulation_unique_id", "snapshot"]].drop_duplicates()
 
-    if len(query_where_clauses) != 0:
-        query += " WHERE " + " AND ".join(query_where_clauses)
+    list_of_results = []
 
-    data = execute_query(query)
-    return pd.DataFrame(data)
+    for _, row in simulation_snapshot_combinations.iterrows():
+        # for each (sim, snap), do query to get profiles
+        simulation_unique_id = row["simulation_unique_id"]
+        snapshot = row["snapshot"]
 
+        query = f"SELECT * FROM profiles WHERE simulation_unique_id = '{simulation_unique_id}' AND snapshot = {snapshot}"
+        if len(list_of_properties) != 0:
+            query += " AND property_key IN (" + ",".join([f"'{p}'" for p in list_of_properties]) + ")"
 
-def get_subhalos(list_of_subhalo_ids, list_of_halo_ids, list_of_simulation_ids, list_of_snapshots, list_of_inequality_filters, list_of_equality_filters):
+        IDs = list_of_halos[
+            (list_of_halos["simulation_unique_id"] == simulation_unique_id) & (list_of_halos["snapshot"] == snapshot)
+        ]["ID"]
+
+        IDs = set(IDs)
+
+        query += " AND ID IN (" + ", ".join([str(i) for i in IDs]) + ")"
+        print(query)
+        data = execute_query(query)
+        result = pd.DataFrame(data)
+        print(result)
+
+        list_of_results.append(result)
+
+    # Combine all results in one DataFrame
+    profiles = pd.concat(list_of_results, ignore_index=True)
+    print(profiles)
+    # Use (simulation_unique_id, snapshot) as the index for the resulting DataFrame
+    profiles = profiles.set_index(["simulation_unique_id", "snapshot"]).sort_index()
+    # to get the index as tuples, use profiles.index.to_numpy()
+
+    return profiles
+
+def get_subhalos_based_on_filters(
+        list_of_subhalos: Union[pd.DataFrame, list[dict]],
+        list_of_inequality_filters: list[INEQUALITY_FILTER],
+        list_of_equality_filters: list[EQUALITY_FILTER]
+    ) -> pd.DataFrame:
+    """Given a list of subhalos and/or lists of desired filters,
+    return the corresponding subhalo entries.
+    
+    Keyword Arguments:
+    list_of_subhalos -- A dict, or pandas.DataFrame, containing the following keys:
+    - subhaloID: subhalo IDs, e.g. 0
+    - simulation_unique_id: e.g. IllustrisTNG_1P_22
+    - snapshot: e.g. 33
+    list_of_inequality_filters: list of filters. Each filter contains a property name, a minimum and a maximum value.
+    - e.g. for one filter: ("M_Crit200", 30, 200)
+    list_of_equality_filters: list of filters. Each filter contains a property name and a list of accepted values.
+    - e.g. for one filter: ("simulation_unique_id", ["IllustrisTNG_1P_22"])
+    """
+
+    # If given a list, convert to pandas.DataFrame, for ease of iteration
+    if isinstance(list_of_subhalos, list):
+        list_of_subhalos = pd.DataFrame(list_of_subhalos)
+
     query = """
     SELECT
     *
     FROM subhalos
     """
 
-    query_where_clauses = []
+    subhalos_where_clauses = []
 
-    if len(list_of_subhalo_ids) != 0:
-        subhalo_in_clause = "subhaloID IN (" + ",".join([f"'{h}'" for h in list_of_subhalo_ids]) + ")"
-        query_where_clauses.append(subhalo_in_clause)
-    if len(list_of_halo_ids) != 0:
-        halo_in_clause = "haloID IN (" + ",".join([f"'{h}'" for h in list_of_halo_ids]) + ")"
-        query_where_clauses.append(halo_in_clause)
-    if len(list_of_simulation_ids) != 0:
-        simulation_in_clause = "simulation_unique_id IN (" + ",".join([f"'{s}'" for s in list_of_simulation_ids]) + ")"
-        query_where_clauses.append(simulation_in_clause)
-    if len(list_of_snapshots) != 0:
-        snapshot_in_clause = "snapshot IN (" + ",".join([f"'{s}'" for s in list_of_snapshots]) + ")"
-        query_where_clauses.append(snapshot_in_clause)
+    for _, subhalo in list_of_subhalos.iterrows():
+        where_clause = f"(subhaloID = {subhalo['subhaloID']} AND simulation_unique_id = '{subhalo['simulation_unique_id']}' AND snapshot = {subhalo['snapshot']})"
+        subhalos_where_clauses.append(where_clause)
+
+    filter_where_clauses = []
+    filter_where_clauses.extend(_build_equality_clause(list_of_equality_filters))
+    filter_where_clauses.extend(_build_inequality_clause(list_of_inequality_filters))
+
+    where_clause = ""
+
+    if len(subhalos_where_clauses) != 0:
+        where_clause += "(" + " OR ".join(subhalos_where_clauses) + ")"
     
-    for (column_name, lower_limit, upper_limit) in list_of_inequality_filters:
-        if lower_limit is not None:
-            query_where_clauses.append(f"{column_name} >= {lower_limit}")
-        if upper_limit is not None:
-            query_where_clauses.append(f"{column_name} <= {upper_limit}")
-
-    for (column_name, values) in list_of_equality_filters:
-        if isinstance(values[0], str):
-            values = [f"'{v}'" for v in values]
-        
-        query_where_clauses.append(f"{column_name} IN ({','.join(values)})")
-
-    if len(query_where_clauses) != 0:
-        query += " WHERE " + " AND ".join(query_where_clauses)
+    if len(filter_where_clauses) != 0:
+        if where_clause != "":
+            where_clause += " AND "
+        where_clause += " AND ".join(filter_where_clauses)
+    
+    if where_clause != "":
+        query += " WHERE " + where_clause
 
     data = execute_query(query)
     return pd.DataFrame(data)
 
-def get_mergertree(starting_subfind_id, starting_snapshot, simulation_unique_id):
+def get_mergertree(
+        starting_subfind_id: int,
+        starting_snapshot: int,
+        simulation_unique_id: str
+    ) -> pd.DataFrame:
+    """Given a simulation, and a starting subhalo at a starting snapshot,
+    return the complete mergertree from the given snapshot going backwards in time.
+
+    Keyword Arguments:
+    starting_subfind_id: Corresponds to the "subhaloID" column in the 'subhalos' table. The subhaloID of the first element of the mergertree.
+    snapshot: The starting snapshot, at which the given subhaloID exists. The mergertree will search backwards in time from this snapshot.
+    simulation_unique_id: e.g. "IllustrisTNG_1P_22". The simulation wherein to search for the mergertree.
+    """
+
     # First, find the subhalo's "MainLeafProgenitorID", which marks the end of the tree
     query = f"""
     SELECT 
@@ -151,22 +248,24 @@ if __name__ == "__main__":
         list_of_equality_filters=[("simulation_unique_id", ["IllustrisTNG_1P_22", "sample ID 2"])],
     )
     print(halos)
-    
-    profiles = get_profiles(
-        list_of_halo_ids=[0, 1],
-        list_of_simulation_ids=["IllustrisTNG_1P_22", "sample ID 2"],
-        list_of_snapshots=[33],
-        list_of_properties=["gas_density"]
+    HUBBLE = 0.6711
+    halos = get_halos_based_on_filters(
+    list_of_inequality_filters=[("M_Crit200", 1/HUBBLE, 1e5/HUBBLE)],
+    list_of_equality_filters=[("simulation_unique_id", ["IllustrisTNG_1P_22", "IllustrisTNG_LH_1"])]
+    )
+
+    list_of_halos = halos[["ID", "simulation_unique_id", "snapshot"]]
+
+    profiles = get_profiles_for_halos(
+        list_of_halos=list_of_halos,
+        list_of_properties=["gas_density", "gas_pressure"]
     )
     print(profiles)
 
-    subhalos = get_subhalos(
-        list_of_subhalo_ids=[290],
-        list_of_halo_ids=[0, 1],
-        list_of_simulation_ids=["IllustrisTNG_1P_22"],
-        list_of_snapshots=[33],
-        list_of_inequality_filters=[],
-        list_of_equality_filters=[]
+    subhalos = get_subhalos_based_on_filters(
+        list_of_subhalos=[],
+        list_of_inequality_filters=[("SubhaloBHMass", 0.01, 0.4)],
+        list_of_equality_filters=[("haloID", [0, 1])]
     )
     print(subhalos)
 
--- sample_end_to_end.py
+++ sample_end_to_end.py
@@ -1,29 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from filter_data_helpers import get_halos_based_on_filters, get_profiles
+from filter_data_helpers import get_halos_based_on_filters, get_profiles_for_halos
 
 HUBBLE = 0.6711
 
 # Select halos based on mass cut and simulation ID
 halo_ids_result = get_halos_based_on_filters(
     list_of_inequality_filters=[("M_Crit200", 1/HUBBLE, 1e5/HUBBLE)],
-    list_of_equality_filters=[("simulation_unique_id", ["IllustrisTNG_1P_22"])]
+    list_of_equality_filters=[("simulation_unique_id", ["IllustrisTNG_1P_22", "IllustrisTNG_LH_1"])]
 )
 print(halo_ids_result)
 
 
 # Select profiles for the filtered halo IDs
-halo_IDs = halo_ids_result["ID"]
-simulation_IDs = halo_ids_result["simulation_unique_id"]
-
-print(halo_IDs)
-print(simulation_IDs)
-
-profiles_result = get_profiles(
-    list_of_halo_ids=halo_IDs,
-    list_of_simulation_ids=simulation_IDs,
-    list_of_snapshots=[33],
-    list_of_properties=["gas_density"] # See options in illstack_helpers.py
+profiles_result = get_profiles_for_halos(
+    list_of_halos=halo_ids_result[["ID", "simulation_unique_id", "snapshot"]],
+    list_of_properties=["gas_density"] # See options in illstack_helpers.py, or enter [] to receive all available profiles
 )
 
 print(profiles_result)
@@ -33,36 +25,53 @@ profiles_result["radius"] *= 1 / (HUBBLE * 1e3) # Convert to Mpc
 profiles_result["property_value"] *= 1e10 * HUBBLE * HUBBLE # Convert to Msun * kpc^-3
 
 # Plot profiles for different halos
-profiles_result = profiles_result.pivot(index="radius", columns="ID", values="property_value")
-print(profiles_result)
 
 # Filter radial values
-profiles_result = profiles_result[(profiles_result.index > 0.01) & (profiles_result.index < 3)]
-
+profiles_result = profiles_result[(profiles_result["radius"] > 0.01) & (profiles_result["radius"] < 3)]
+print(profiles_result)
 
 # Bin halo_IDs by M_Crit200
 mass_ranges = [(1, 2), (2, 10), (10, 100), (100, 1000)]
-label_mass_ranges = [(1e10*low/HUBBLE, 1e10*high/HUBBLE) for (low, high) in mass_ranges]
 halo_IDs_bins = [list(halo_ids_result[(halo_ids_result["M_Crit200"] >= low) & (halo_ids_result["M_Crit200"] < high)]["ID"]) for (low, high) in mass_ranges]
 print(halo_IDs_bins)
+
+# Convert units
+label_mass_ranges = [(1e10*low/HUBBLE, 1e10*high/HUBBLE) for (low, high) in mass_ranges]
+
+# Obtain all unique (simulation, snapshot) combinations in profiles dataset
+sim_snap_combinations = profiles_result.index.drop_duplicates().to_numpy()
 
 # Compute and plot percentiles of profile data
 fig, ax = plt.subplots(1, 1, figsize=(8, 5))
 
-
-for mass_range, halo_IDs_bin in zip(label_mass_ranges, halo_IDs_bins):
+for mass_range in label_mass_ranges:
     low, high = np.log10(mass_range)
 
-    binned_profiles = profiles_result[halo_IDs_bin]
-    print(binned_profiles)
+    halos_filtered = halo_ids_result[(halo_ids_result["M_Crit200"] >= low) & (halo_ids_result["M_Crit200"] < high)]
 
-    profiles_16 = binned_profiles.quantile(q=0.16, axis="columns")
-    profiles_84 = binned_profiles.quantile(q=0.84, axis="columns")
-    profiles_median = binned_profiles.quantile(q=0.5, axis="columns")
-    print(profiles_16)
+    for sim, snapshot in sim_snap_combinations:
+        # Determine halo IDs within this simulation and snapshot
+        sim_snap_halo_ids = halos_filtered[(halos_filtered["simulation_unique_id"] == sim) & (halos_filtered["snapshot"] == snapshot)]["ID"]
+        # Determine profiles for this simulation and snapshot (use .loc since (simulation, snapshot) is the MultiIndex for the profiles DataFrame)
+        sim_snap_profiles = profiles_result.loc[(sim, snapshot)]
+        # Filter profiles based on halo IDs
+        sim_snap_profiles = sim_snap_profiles[sim_snap_profiles["ID"].isin(sim_snap_halo_ids)]
+        # Only keep radius and property_value columns
+        sim_snap_profiles = sim_snap_profiles[["radius", "property_value"]]
 
-    ax.fill_between(profiles_16.index, profiles_16, profiles_84, alpha=0.2)
-    profiles_median.plot(label=f"{low:.1f} <=" + r" $log_{10} (M_{200c} / M_{\odot}) <$" + f" {high:.1f}")
+        # Determine median and percentiles for the profile data, and plot
+        median_profiles = sim_snap_profiles.groupby("radius", as_index=False).median()
+
+        ax.plot(
+            median_profiles["radius"],
+            median_profiles["property_value"],
+            label=f"{sim} / {snapshot}, Mass within ({low:.1f} {high:.1f})"
+        )
+
+        # Display quantiles
+        profiles_16 = sim_snap_profiles.groupby("radius", as_index=False).quantile(q=0.16)
+        profiles_84 = sim_snap_profiles.groupby("radius", as_index=False).quantile(q=0.84)
+        ax.fill_between(profiles_16["radius"], profiles_16["property_value"], profiles_84["property_value"], alpha=0.2)
 
 # Display plot
 ax.set_xscale("log")
